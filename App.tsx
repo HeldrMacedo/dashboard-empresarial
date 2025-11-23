89b95a1446286c82391facfaf5ea3635e866cd44
Helder Macedo <heldr.macedo@gmail.com>
2025-11-23 19:53:06 -0300

    altera ordem dos cards

--- App.tsx
+++ App.tsx
@@ -215,7 +215,7 @@ const App: React.FC = () => {
           )}
           
           {!loading && (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               <SummaryCard title="Total Notas" value={globalSummary.total_notas} icon={DollarSign} colorClass="bg-blue-600" />
               <SummaryCard title="Total Cancelados" value={globalSummary.total_cancelados} icon={AlertCircle} colorClass="bg-red-500" />
               <SummaryCard title="Total Devolvido" value={globalSummary.total_devolvido} icon={Archive} colorClass="bg-orange-500" />
